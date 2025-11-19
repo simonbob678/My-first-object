ca1d0f65f9389875155ed0461d1bc246404349e5
simonbob678 <3437759879@qq.com>
2025-11-19 12:33:03 +0800

    Delete src directory
    123

--- src/views/CategoryPage.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="page">
-    <h1>这是分类页面</h1> <!-- 必须有可见内容 -->
-    <p></p>
-  </div>
-</template>
--- src/views/ContactPage.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="category-page">
-    <h1>这是联系页面</h1> <!-- 必须有可见内容 -->
-    <p></p>
-  </div>
-</template>
